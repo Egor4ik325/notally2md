c6136a3d383e8454966a227e3eecd49c0077a742
Egor4ik325 <zorinegor325@gmail.com>
2021-08-31 20:34:08 +0300

    Write note/list body to the file

--- convert.py
+++ convert.py
@@ -10,20 +10,42 @@ def create_notes(base_path, notes, dir_name):
     notes_notes = notes.get('note')
     if notes_notes:
         for i, note in enumerate(notes_notes):
-            note_path = notes_path / f'note_{i}.txt'
+            note_path = notes_path / f'note_{i}.md'
             with open(note_path, 'wt') as f:
                 note_title = note.get('title')
                 if note_title:
-                    f.write(note_title)
+                    f.write(f'# {note_title }\n\n')
+                note_body = note.get('body')
+                if note_body:
+                    f.write(note_body)
 
     notes_lists = notes.get('list')
     if notes_lists:
         for i, lst in enumerate(notes_lists):
-            list_path = notes_path / f'list_{i}.txt'
+            list_path = notes_path / f'list_{i}.md'
             with open(list_path, 'wt') as f:
                 list_title = lst.get('title')
                 if list_title:
-                    f.write(list_title)
+                    f.write(f'# {list_title}\n\n')
+                # Write every list item
+                list_items = lst.get('item')
+                if list_items:
+                    for list_item in list_items:
+                        if isinstance(list_item, str):
+                            list_item = list_items
+
+                        list_item_text = list_item.get('text')
+                        list_item_checked = list_item.get('checked')
+                        if list_item_text:
+                            if list_item_checked is None:
+                                f.write(f'- {list_item_text}\n')
+                            elif list_item_checked.lower() == 'true':
+                                f.write(f'- [x] {list_item_text}\n')
+                            else:
+                                f.write(f'- [ ] {list_item_text}\n')
+
+                        if isinstance(list_item, str):
+                            break
 
 
 if __name__ == "__main__":
